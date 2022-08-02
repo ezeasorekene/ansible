19be1d99fb8e454886436721ee03c99a44093356
Ekene Ezeasor <ezeasorekene@gmail.com>
2022-08-02 13:33:37 +0100

    Fixed directory location

--- aws_ansible/saveHost.sh
+++ aws_ansible/saveHost.sh
@@ -1,16 +1,23 @@
 #!/bin/bash 
 
-dir="$(dirname -- "$(readlink -f "${BASH_SOURCE}")")"
+# dir="$(dirname -- "$(readlink -f "${BASH_SOURCE}")")"
+dir=aws_ansible
 
-if grep -Fxq "$1" inventory
+
+if [[ ! -f ${dir}/inventory ]]
+then
+    exit 1
+fi
+
+if grep -Fxq "$1" ${dir}/inventory
 then
     echo "Host already exits" \
     && exit 1
 else
     if [[ -z ${2} ]];
     then
-        echo $1 >> aws_ansible/inventory
+        echo $1 >> ${dir}/inventory
     else
-        echo $1 >> aws_ansible/$2
+        echo $1 >> ${dir}/$2
     fi
 fi
