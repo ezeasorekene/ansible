81ef0661841c6c238225baa606eaf557e84fcc5f
Ekene Ezeasor <ezeasorekene@gmail.com>
2022-08-02 14:08:00 +0100

    Edited config files

--- aws_ansible/saveHost.sh
+++ aws_ansible/saveHost.sh
@@ -1,23 +1,25 @@
 #!/bin/bash 
 
 # dir="$(dirname -- "$(readlink -f "${BASH_SOURCE}")")"
-dir=aws_ansible
+# dir=aws_ansible
+dir=~
+file=inventory.txt
 
 
-if [[ ! -f ${dir}/inventory ]]
+if [[ ! -f ${dir}/${file} ]]
 then
     exit 1
 fi
 
-if grep -Fxq "$1" ${dir}/inventory
+if grep -Fxq "$1" ${dir}/${file}
 then
     echo "Host already exits" \
     && exit 1
 else
     if [[ -z ${2} ]];
     then
-        echo $1 >> ${dir}/inventory \
-        cat ${dir}/inventory
+        echo $1 >> ${dir}/${file} \
+        cat ${dir}/${file}
     else
         echo $1 >> ${dir}/$2 \
         && cat ${dir}/$2
